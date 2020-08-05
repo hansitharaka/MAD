9fd8c0b6342d3df60bdabfe2ddf6721be495292e
HansaniTharaka <66997469+HansaniTharaka@users.noreply.github.com>
2020-08-06 01:31:13 +0530

    Activity Life Cycles

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         TextView txtMsg2 = findViewById(R.id.txtMsg2);
         txtMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle", "OnCreate() invoked");
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("Lifecycle", "onStart() invoked");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart() invoked");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("Lifecycle", "onResume() invoked");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("Lifecycle", "onPause() invoked");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("Lifecycle", "onStop() invoked");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle", "onDestroy() invoked");
+    }
+
 }
